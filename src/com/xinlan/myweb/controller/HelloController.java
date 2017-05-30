b313c20a6a266434368d1a022c0817414392dda5
siwangqishiq <@LAPTOP-4O3P6OCD>
2017-05-30 14:35:48 +0800

    init

---
+++ src/com/xinlan/myweb/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.xinlan.myweb.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, 
+			HttpServletResponse response) throws Exception {
+		
+		
+		return null;
+	}
+}//end class
